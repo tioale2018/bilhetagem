bc19f063905518904deaeb6944cf4950a8e4fe73
tioale2018 <ale.tpd@gmail.com>
2024-07-11 12:59:23 -0300

    atualiza conexao

--- admin/inc/conexao.php
+++ admin/inc/conexao.php
@@ -1,17 +1,23 @@
 <?php
-$servername = "localhost";
+ $servername = "localhost";
  $server_name = $_SERVER['SERVER_NAME'];
 
-if ($server_name == 'localhost' || $server_name == '127.0.0.1' || $server_name == '192.168.2.16') {
+ if ($server_name == 'localhost' || $server_name == '127.0.0.1' || $server_name == '192.168.2.16') {
   # code...
   $username = "root";
   $password = "";
   $database = "bdbilhetagem";
-} else {
+} elseif ($server_name == 'w3brand.com.br') {
+  //homologa w3brand
   # code...
   $username = "brandw3com_userbilhetagem";
   $password = "@}T)Cupw0e_z";
   $database = "brandw3com_bdbilhetagem";
+} elseif ($server_name == 'rapidticket.com.br') {
+  //produção rapidticket
+  $username = "rapidcom_userbilhetagem";
+  $password = "i#U9ZPP-IB3V";
+  $database = "rapidcom_bdbilhetagem";
 }
 
 date_default_timezone_set('America/Sao_Paulo');
