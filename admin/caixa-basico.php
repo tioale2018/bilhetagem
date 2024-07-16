5f37b3df8bfb5e209fd596969bf89b854a56febc
tioale2018 <ale.tpd@gmail.com>
2024-07-15 21:45:33 -0300

    Update caixa-basico.php

--- admin/caixa-basico.php
+++ admin/caixa-basico.php
@@ -130,7 +130,7 @@ try {
         <th><?= $value['id_prevenda'] ?></th>
         <th><?= $tpcobranca[$value['tp_cobranca']] ?></th>
         <th><?= $formapgto[$value['forma_pgto']] ?></th>
-        <th><?= date('H:i:s', $value['hora_pgto']) ?></th>
+        <th><?= date('H:i', $value['hora_pgto']) ?></th>
         <th>R$ <?= number_format($value['valor'], 2, ',', '.') ?></th>
     </tr>
         <?php
