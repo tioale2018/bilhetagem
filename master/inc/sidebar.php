7cfdcc72a159d68aaa332321c0d1f32dac2d1a32
tioale2018 <ale.tpd@gmail.com>
2024-10-25 01:44:32 -0300

    atualiza

---
+++ master/inc/sidebar.php
@@ -0,0 +1,322 @@
+<aside id="minileftbar" class="minileftbar">
+    <ul class="menu_list">
+        <li>
+            <a href="javascript:void(0);" class="bars"></a>
+            <a class="navbar-brand" href="index.php"><img src="assets/images/logo.svg" alt="Alpino"></a>
+        </li>
+        <!-- <li><a href="javascript:void(0);" class="btn_overlay hidden-sm-down"><i class="zmdi zmdi-search"></i></a></li>         -->
+        <li><a href="javascript:void(0);" class="menu-sm"><i class="zmdi zmdi-swap"></i></a></li>        
+        <!-- <li class="menuapp-btn"><a href="javascript:void(0);"><i class="zmdi zmdi-apps"></i></a></li> -->
+        <!-- <li class="notifications badgebit">
+            <a href="javascript:void(0);">
+                <i class="zmdi zmdi-notifications"></i>
+                <div class="notify">
+                    <span class="heartbit"></span>
+                    <span class="point"></span>
+                </div>
+            </a>
+        </li> -->
+        <li class="task badgebit">
+            <a href="javascript:void(0);">
+                <i class="zmdi zmdi-flag"></i>
+                <div class="notify">
+                    <span class="heartbit"></span>
+                    <span class="point"></span>
+                </div>
+            </a>
+        </li>
+        
+        <li class="power">
+            <a href="javascript:void(0);" class="js-right-sidebar"><i class="zmdi zmdi-settings zmdi-hc-spin"></i></a>            
+            <a href="sign-in.html" class="mega-menu"><i class="zmdi zmdi-power"></i></a>
+        </li>
+    </ul>    
+</aside>
+
+
+
+
+<aside class="right_menu">
+    
+    <div class="task-menu">
+        <div class="slim_scroll">
+            <div class="card tasks">
+                <div class="header">
+                    <h2>Eventos ativos</h2>
+                </div>
+                <div class="body m-b-10">
+                    <a href="javascript:void(0);">
+                        <span class="text-muted">Park Shopping Canoas <span class="float-right">29%</span></span>
+                        <div class="progress">
+                            <div class="progress-bar l-turquoise" role="progressbar" aria-valuenow="29" aria-valuemin="0" aria-valuemax="100" style="width: 29%;"></div>
+                        </div>
+                    </a>
+                </div>
+                <div class="body m-b-10">
+                    <a href="javascript:void(0);">
+                        <span class="text-muted">Shopping Guararapes <span class="float-right">78%</span></span>
+                        <div class="progress">
+                            <div class="progress-bar l-slategray" role="progressbar" aria-valuenow="78" aria-valuemin="0" aria-valuemax="100" style="width: 78%;"></div>
+                        </div>
+                    </a>
+                </div>
+                <div class="body">
+                    <a href="javascript:void(0);">
+                        <span class="text-muted">Shopping Metropolitano <span class="float-right">68%</span></span>
+                        <div class="progress">
+                            <div class="progress-bar l-coral" role="progressbar" aria-valuenow="68" aria-valuemin="0" aria-valuemax="100" style="width: 68%;"></div>
+                        </div>
+                    </a>
+                </div>
+
+                
+            </div>
+        </div>
+    </div>
+    <div id="rightsidebar" class="right-sidebar">
+        <ul class="nav nav-tabs">
+            <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#setting">Setting</a></li>        
+            <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#activity">Activity</a></li>
+        </ul>
+        <div class="tab-content slim_scroll">
+            <div class="tab-pane slideRight active" id="setting">
+                <div class="card">
+                    <div class="header">
+                        <h2><strong>Colors</strong> Skins</h2>
+                    </div>
+                    <div class="body">
+                        <ul class="choose-skin list-unstyled m-b-0">
+                            <li data-theme="black" class="active">
+                                <div class="black"></div>
+                            </li>
+                            <li data-theme="purple">
+                                <div class="purple"></div>
+                            </li>                   
+                            <li data-theme="blue">
+                                <div class="blue"></div>
+                            </li>
+                            <li data-theme="cyan">
+                                <div class="cyan"></div>                    
+                            </li>
+                            <li data-theme="green">
+                                <div class="green"></div>
+                            </li>
+                            <li data-theme="orange">
+                                <div class="orange"></div>
+                            </li>
+                            <li data-theme="blush">
+                                <div class="blush"></div>                    
+                            </li>
+                        </ul>
+                    </div>
+                </div>                
+                <div class="card">
+                    <div class="header">
+                        <h2><strong>General</strong> Settings</h2>
+                    </div>
+                    <div class="body">
+                        <ul class="setting-list list-unstyled m-b-0">
+                            <li>
+                                <div class="checkbox">
+                                    <input id="checkbox1" type="checkbox">
+                                    <label for="checkbox1">Report Panel Usage</label>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="checkbox">
+                                    <input id="checkbox2" type="checkbox" checked="">
+                                    <label for="checkbox2">Email Redirect</label>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="checkbox">
+                                    <input id="checkbox3" type="checkbox">
+                                    <label for="checkbox3">Notifications</label>
+                                </div>                        
+                            </li>
+                            <li>
+                                <div class="checkbox">
+                                    <input id="checkbox4" type="checkbox">
+                                    <label for="checkbox4">Auto Updates</label>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="checkbox">
+                                    <input id="checkbox5" type="checkbox" checked="">
+                                    <label for="checkbox5">Offline</label>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="checkbox m-b-0">
+                                    <input id="checkbox6" type="checkbox">
+                                    <label for="checkbox6">Location Permission</label>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="header">
+                        <h2><strong>Left</strong> Menu</h2>
+                    </div>
+                    <div class="body theme-light-dark">
+                        <button class="t-dark btn btn-primary btn-round btn-block">Dark</button>
+                    </div>
+                </div>               
+            </div>
+            <div class="tab-pane slideLeft" id="activity">
+                <div class="card activities">
+                    <div class="header">
+                        <h2><strong>Recent</strong> Activity Feed</h2>
+                    </div>
+                    <div class="body">
+                        <div class="streamline b-accent">
+                            <div class="sl-item">
+                                <div class="sl-content">
+                                    <div class="text-muted">Just now</div>
+                                    <p>Finished task <a href="" class="text-info">#features 4</a>.</p>
+                                </div>
+                            </div>
+                            <div class="sl-item b-info">
+                                <div class="sl-content">
+                                    <div class="text-muted">10:30</div>
+                                    <p><a href="">@Jessi</a> retwit your post</p>
+                                </div>
+                            </div>
+                            <div class="sl-item b-primary">
+                                <div class="sl-content">
+                                    <div class="text-muted">12:30</div>
+                                    <p>Call to customer <a href="" class="text-info">Jacob</a> and discuss the detail.</p>
+                                </div>
+                            </div>
+                            <div class="sl-item b-warning">
+                                <div class="sl-content">
+                                    <div class="text-muted">1 days ago</div>
+                                    <p><a href="" class="text-info">Jessi</a> commented your post.</p>
+                                </div>
+                            </div>
+                            <div class="sl-item b-primary">
+                                <div class="sl-content">
+                                    <div class="text-muted">2 days ago</div>
+                                    <p>Call to customer <a href="" class="text-info">Jacob</a> and discuss the detail.</p>
+                                </div>
+                            </div>
+                            <div class="sl-item b-primary">
+                                <div class="sl-content">
+                                    <div class="text-muted">3 days ago</div>
+                                    <p>Call to customer <a href="" class="text-info">Jacob</a> and discuss the detail.</p>
+                                </div>
+                            </div>
+                            <div class="sl-item b-warning">
+                                <div class="sl-content">
+                                    <div class="text-muted">4 Week ago</div>
+                                    <p><a href="" class="text-info">Jessi</a> commented your post.</p>
+                                </div>
+                            </div>
+                            <div class="sl-item b-warning">
+                                <div class="sl-content">
+                                    <div class="text-muted">5 days ago</div>
+                                    <p><a href="" class="text-info">Jessi</a> commented your post.</p>
+                                </div>
+                            </div>
+                            <div class="sl-item b-primary">
+                                <div class="sl-content">
+                                    <div class="text-muted">5 Week ago</div>
+                                    <p>Call to customer <a href="" class="text-info">Jacob</a> and discuss the detail.</p>
+                                </div>
+                            </div>
+                            <div class="sl-item b-primary">
+                                <div class="sl-content">
+                                    <div class="text-muted">3 Week ago</div>
+                                    <p>Call to customer <a href="" class="text-info">Jacob</a> and discuss the detail.</p>
+                                </div>
+                            </div>
+                            <div class="sl-item b-warning">
+                                <div class="sl-content">
+                                    <div class="text-muted">1 Month ago</div>
+                                    <p><a href="" class="text-info">Jessi</a> commented your post.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div id="leftsidebar" class="sidebar">
+        <div class="menu">
+            <ul class="list">
+                <li>
+                    <div class="user-info m-b-20">
+                        <div class="image">
+                            <a href="profile.html"><img src="assets/images/profile_av.jpg" alt="User"></a>
+                        </div>
+                        <div class="detail">
+                            <h6>Rapidticket</h6>
+                            <p class="m-b-0">Sistemas de Gestão de Bilhetagem</p>
+                            <a href="javascript:void(0);" title="" class=" waves-effect waves-block"><i class="zmdi zmdi-facebook-box"></i></a>
+                            <a href="javascript:void(0);" title="" class=" waves-effect waves-block"><i class="zmdi zmdi-linkedin-box"></i></a>
+                            <a href="javascript:void(0);" title="" class=" waves-effect waves-block"><i class="zmdi zmdi-instagram"></i></a>
+                            <a href="javascript:void(0);" title="" class=" waves-effect waves-block"><i class="zmdi zmdi-twitter-box"></i></a>                            
+                        </div>
+                    </div>
+                </li>
+                <li class="header">MENU</li>
+                <li class="active open"> <a href="index.php"><i class="zmdi zmdi-home"></i><span>Dashboard</span></a></li>
+                <li><a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-apps"></i><span>Ger. Eventos</span> <span class="badge badge-success float-right">7</span></a>
+                    <ul class="ml-menu">
+                        <li><a href="mail-inbox.html">Novo Evento</a></li>
+                        <li><a href="chat.html">Lista evento</a></li>
+                        <li><a href="events.html">Calendar</a></li>
+                        <li><a href="file-dashboard.html">File Manager</a></li>
+                        <li><a href="contact.html">Contact list</a></li>
+                        <li><a href="blog-dashboard.html">Blog</a></li>
+                        <li><a href="app-ticket.html">Support Ticket</a></li>
+                    </ul>
+                </li>
+                <li><a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-swap-alt"></i><span>Usuários</span> <span class="badge badge-default float-right">15</span></a>
+                    <ul class="ml-menu">
+                        <li><a href="ui_kit.html">Novo Usuário</a></li>                    
+                        <li><a href="alerts.html">Listar/Editar</a></li>                    
+                        <li><a href="collapse.html">Acessos</a></li>
+                    </ul>
+                </li>
+                <li><a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-assignment"></i><span>Responsáveis</span> <span class="badge badge-warning float-right">9</span></a>
+                    <ul class="ml-menu">
+                        <li><a href="basic-form-elements.html">Basic Elements</a></li>
+                        <li><a href="advanced-form-elements.html">Advanced Elements</a></li>
+                        <li><a href="form-examples.html">Form Examples</a></li>
+                        <li><a href="form-validation.html">Form Validation</a></li>
+                        <li><a href="form-wizard.html">Form Wizard</a></li>
+                        <li><a href="form-editors.html">Editors</a></li>
+                        <li><a href="form-upload.html">File Upload</a></li>
+                        <li><a href="form-img-cropper.html">Image Cropper</a></li>
+                        <li><a href="form-summernote.html">Summernote</a></li>
+                    </ul>
+                </li>
+                <li><a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-grid"></i><span>Relatórios</span> <span class="badge badge-info float-right">6</span></a>
+                    <ul class="ml-menu">                        
+                        <li><a href="normal-tables.html">Normal Tables</a></li>
+                        <li><a href="jquery-datatable.html">Jquery Datatables</a></li>
+                        <li><a href="editable-table.html">Editable Tables</a></li>
+                        
+                        <li><a href="table-color.html">Tables Color</a></li>
+						<li><a href="table-filter.html">Tables Filter</a></li>
+                    </ul>
+                </li>   
+                         
+                <li><a href="javascript:void(0);" class="menu-toggle"><i class="zmdi zmdi-chart"></i><span>Faturas</span> <span class="badge badge-default float-right">6</span></a>
+                    <ul class="ml-menu">
+                        <li><a href="morris.html">Morris</a></li>
+                        <li><a href="flot.html">Flot</a></li>
+                        <li><a href="chartjs.html">ChartJS</a></li>
+                        <li><a href="sparkline.html">Sparkline</a></li>
+                        <li><a href="jquery-knob.html">Jquery Knob</a></li>
+                            <li><a href="chart-e.html">E Chart</a></li>
+                    </ul>
+                </li>
+               
+            </ul>
+        </div>
+    </div>
+</aside>
