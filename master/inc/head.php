18e31cf3e8274fd0b0925cad04139d289301dc7a
tioale2018 <ale.tpd@gmail.com>
2024-12-01 01:52:56 -0300

    master

--- master/inc/head.php
+++ master/inc/head.php
@@ -11,16 +11,19 @@ verificarSessao();
 <meta http-equiv="X-UA-Compatible" content="IE=Edge">
 <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 <meta name="description" content="Responsive Bootstrap 4 and web Application ui kit.">
-<title>:: Alpino :: Home</title>
+<title>Sistema de Bilhetagem</title>
 <link rel="icon" href="favicon.ico" type="image/x-icon"> <!-- Favicon-->
-<link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
-<link rel="stylesheet" href="assets/plugins/morrisjs/morris.css" />
-<link rel="stylesheet" href="assets/plugins/jvectormap/jquery-jvectormap-2.0.3.min.css"/>
+<link rel="stylesheet" href="./assets/plugins/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="./assets/plugins/morrisjs/morris.css" />
+<link rel="stylesheet" href="./assets/plugins/jvectormap/jquery-jvectormap-2.0.3.min.css"/>
 <!-- Custom Css -->
-<link rel="stylesheet" href="assets/css/main.css">
-<link rel="stylesheet" href="assets/css/color_skins.css">
+<link rel="stylesheet" href="./assets/css/main.css">
+<link rel="stylesheet" href="./assets/css/color_skins.css">
 <!-- Jquery Core Js -->
-<script src="assets/bundles/libscripts.bundle.js"></script> <!-- Lib Scripts Plugin Js ( jquery.v3.2.1, Bootstrap4 js) -->
-<script src="assets/bundles/vendorscripts.bundle.js"></script> <!-- slimscroll, waves Scripts Plugin Js -->
+<script src="./assets/bundles/libscripts.bundle.js"></script> <!-- Lib Scripts Plugin Js ( jquery.v3.2.1, Bootstrap4 js) -->
+<script src="./assets/bundles/vendorscripts.bundle.js"></script> <!-- slimscroll, waves Scripts Plugin Js -->
 <link rel="stylesheet" href="./assets/plugins/sweetalert/sweetalert.css">
 <script src="./assets/plugins/sweetalert/sweetalert.min.js"></script>
+<script src="./assets/plugins/jquery-mask/jquery.mask.min.js"></script>
+<link href="./assets/plugins/bootstrap-select/css/bootstrap-select.css" rel="stylesheet" />
+
--- master/inc/javascript.php
+++ master/inc/javascript.php
@@ -1,8 +1,9 @@
-<script src="assets/bundles/knob.bundle.js"></script> <!-- Jquery Knob-->
-<script src="assets/bundles/jvectormap.bundle.js"></script> <!-- JVectorMap Plugin Js -->
-<script src="assets/bundles/morrisscripts.bundle.js"></script> <!-- Morris Plugin Js --> 
-<script src="assets/bundles/sparkline.bundle.js"></script> <!-- sparkline Plugin Js --> 
-<script src="assets/bundles/doughnut.bundle.js"></script>
+<script src="./assets/bundles/knob.bundle.js"></script> <!-- Jquery Knob-->
+<script src="./assets/bundles/jvectormap.bundle.js"></script> <!-- JVectorMap Plugin Js -->
+<script src="./assets/bundles/morrisscripts.bundle.js"></script> <!-- Morris Plugin Js --> 
+<script src="./assets/bundles/sparkline.bundle.js"></script> <!-- sparkline Plugin Js --> 
+<script src="./assets/bundles/doughnut.bundle.js"></script>
 
-<script src="assets/bundles/mainscripts.bundle.js"></script>
-<script src="assets/js/pages/index.js"></script>
+<script src="./assets/bundles/mainscripts.bundle.js"></script>
+<script src="./assets/js/pages/index.js"></script>
+<script src="./assets/plugins/bootstrap-select/js/bootstrap-select.js"></script>
