c4eb54ae58bee5bdf62d57ba51209ad8111a79e7
tioale2018 <ale.tpd@gmail.com>
2025-01-29 22:01:17 -0300

    relata periodo

---
+++ admin/caixa-periodo-produtos.php
@@ -0,0 +1,179 @@
+<?php include_once('./inc/head.php') ?>
+<?php include_once('./inc/conexao.php') ?>
+<?php include_once('./inc/funcao-tempo.php') ?>
+<?php include_once('./inc/funcoes-calculo.php') ?>
+<?php include_once('./inc/funcoes.php') ?>
+
+<?php
+function generateSqlQuery($dataI, $dataF) {
+    $dateInicial = DateTime::createFromFormat('Y-m-d', $dataI);
+    $dateFinal = DateTime::createFromFormat('Y-m-d', $dataF);
+
+    if ($dateInicial === false || $dateFinal === false) {
+        throw new Exception('Data inválida. Use o formato YYYY-MM-DD.');
+    }
+    $startTimestamp = $dateInicial->setTime(0, 0)->getTimestamp();
+    $endTimestamp = $dateFinal->setTime(23, 59, 59)->getTimestamp();
+   // $sql = "SELECT count(id_pacote) as total_vendido, pct_nome, pct_valor, pct_duracao FROM tbentrada where datahora_entra BETWEEN {$startTimestamp} AND {$endTimestamp} group by id_pacote order by total_vendido";
+
+    $sql = "SELECT count(tbentrada.id_pacote) as total_vendido, tbentrada.pct_nome, tbentrada.pct_valor, tbentrada.pct_duracao, tbprevenda.id_evento FROM tbentrada inner join tbprevenda on tbentrada.id_prevenda=tbprevenda.id_prevenda where tbentrada.id_pacote>0 and tbprevenda.id_evento=".$_SESSION['evento_selecionado']." and tbentrada.datahora_entra BETWEEN  {$startTimestamp} AND {$endTimestamp} group by tbentrada.id_pacote order by tbentrada.pct_nome";
+    // die($sql);
+    // $sql = "SELECT * FROM tbfinanceiro WHERE ativo=1 AND hora_pgto BETWEEN ";
+    return $sql;
+}
+
+$varget = '';
+  
+$inicialData = date('Y-m-d');
+$finalData   = date('Y-m-d');
+  
+if (isset($_POST['di'])) {
+    //validar data
+    $inicialData = $_POST['di'];
+    $finalData = $_POST['df'];
+} else {
+    if (isset($_GET['di'])) {
+        //validar data
+        $inicialData = $_GET['di'];
+        $finalData = $_GET['df'];
+    }         
+}
+
+$dataRelataIni = $inicialData;
+$dataRelataFim = $finalData;
+
+$varget = '?di='.$dataRelataIni.'&df='.$dataRelataFim;
+
+    $sql_busca_pgto = generateSqlQuery($inicialData, $finalData);
+    //die("<pre>".$sql_busca_pgto."</pre>");
+    $pre_busca_pgto = $connPDO->prepare($sql_busca_pgto);
+    $pre_busca_pgto->execute();
+    $row_busca_pgto = $pre_busca_pgto->fetchAll();
+
+?>
+
+</head>
+<body class="theme-black">
+<?php //include_once('./inc/pageloader.php') ?>
+
+<?php include_once('./inc/menu_topo.php') ?>
+<?php include_once('./inc/menu_principal.php') ?>
+<?php include_once('./inc/menu_lateral.php') ?>
+
+<section class="content">    
+    <div class="container">
+        <div class="block-header">
+            <div class="row clearfix">
+                <div class="col-lg-5 col-md-5 col-sm-12 mt-4">
+                    <h2>Pacotes vendidos</h2>        
+                </div>
+            </div>
+        </div>
+
+        <div class="row clearfix">
+            <div class="col-lg-12">
+                <div class="card" id="details">
+                    <div class="body">  
+                        
+                    <form action="" method="post">
+                            <div class="row">
+                                
+
+                                
+                                <div class="col-md-7 col-sm-6">
+                                    <div class="my-2 row">
+                                        <div class="col-md-2"><strong>Data Inicial:</strong></div> 
+                                        <div class="col-md-6"><input class="form-control" type="date" name="di" id="dataFiltro" max="<?= date('Y-m-d', time()) ?>" value="<?= $dataRelataIni ?>"></div> 
+                                    </div>
+                                    <div class="my-2 row">
+                                            <div class="col-md-2"><strong>Data Final:</strong></div> 
+                                            <div class="col-md-6"><input class="form-control" type="date" name="df" id="dataFiltro" max="<?= date('Y-m-d', time()) ?>" value="<?= $dataRelataFim ?>"></div> 
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <p style="text-align: right"><button type="submit" class="btn btn-primary">Ok</button></p>
+
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+
+                                
+
+                                <div class="col-md-5 col-sm-6">
+                                    <p>
+                                        <a href="caixa-periodo-tipopgto<?= $varget ?>">Por tipo de pagamento</a> | <a href="caixa-periodo-produtos<?= $varget ?>">Pacotes vendidos</a>
+                                    </p>
+                                </div>
+                                
+                            </div>
+                        </form>
+                        <div class="mt-40"></div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="table-responsive tabela-caixa">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>                                                  
+                            <th>Nome do pacote</th>
+                            <th>Qtde. vendida</th>
+                            <th>Valor</th>
+                            <th>Subtotal</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php if($pre_busca_pgto->rowCount() < 1) { ?>
+                            <tr>
+                                <td colspan="7" style="text-align: center">Nenhum resultado encontrado</td>
+                            </tr>
+                    <?php } else { 
+                        $subtotal = 0;
+                        $total    = 0;
+                        foreach ($row_busca_pgto as $key => $value) {
+                            $subtotal = $value['pct_valor'] * $value['total_vendido'];
+                            $total = $total + $subtotal;
+                            ?>
+                        <tr>
+                            <th><?= $value['pct_nome'] ?></th>
+                            <th><?= $value['total_vendido'] ?></th>       
+                            <th>R$ <?= number_format($value['pct_valor'], 2, ',', '.') ?></th>
+                            <th>R$ <?= number_format($subtotal, 2, ',', '.') ?></th>
+                        </tr>
+                            <?php
+                        }
+
+                    } ?>
+                            </tbody>
+                </table>
+                <?php if (isset($total)) { ?>
+                <p>Total: R$ <?= number_format($total, 2, ',', '.') ?></p>
+                <?php } ?>
+                                </div>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</section>
+
+<?php include_once('./inc/javascript.php') ?>
+
+<script>
+    $(document).ready(function(){
+        $('form').submit(function(e){
+            let di = $('input[name="di"]').val();
+            let df = $('input[name="df"]').val();
+            if (di>df) {
+                e.preventDefault();
+                alert('Data inicial deve ser inferior ou igual a data final');
+            }
+        });
+    });
+</script>
+
+</body>
+</html>
--- admin/inc/menu_principal.php
+++ admin/inc/menu_principal.php
@@ -34,6 +34,7 @@
                     <li><a href="javascript:void(0)">Gerencial</a>
                         <ul class="sub-menu">
                             <li><a href="caixa-tipopgto">Pagamentos</a></li>
+                            <li><a href="caixa-periodo-tipopgto">Pagamentos por período</a></li>
                             <li><a href="caixa-fechamento">Fechamento de caixa</a></li>
                             <li><a href="termo-exporta">Exporta termo</a></li>
                         </ul>
