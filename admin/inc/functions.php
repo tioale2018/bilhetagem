c1ad02561ead17a0688c7a7b35fda4a4d3c4b01c
tioale2018 <ale.tpd@gmail.com>
2025-04-01 23:51:41 -0300

    resolve redirect

--- admin/inc/functions.php
+++ admin/inc/functions.php
@@ -3,7 +3,8 @@
 
 function verificarSessao() {
     // Verifica se a variável de sessão 'user_id' está definida
-    if ((!isset($_SESSION['user_id'])) && $_SESSION['user_perfil']!=1) {
+    // if ((!isset($_SESSION['user_id'])) && $_SESSION['user_perfil']!=1) {  - versão original
+    if ( !isset($_SESSION['user_id']) ) { // versão para resolver o problema de redirecionamento
         // Se não estiver definida, redireciona para a página de login
         session_unset();
         session_destroy();
