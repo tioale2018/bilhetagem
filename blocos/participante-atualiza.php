1cdda8b24e206a147a6834921d6d0c7b5e77983c
tioale2018 <ale.tpd@gmail.com>
2025-04-02 10:46:46 -0300

    Update participante-atualiza.php

--- blocos/participante-atualiza.php
+++ blocos/participante-atualiza.php
@@ -28,29 +28,27 @@ $pre = $connPDO->prepare($sql);
 $pre->bindParam(':idvinculado', $idvinculado, PDO::PARAM_INT);
 // $pre->bindParam(':idresponsavel', $idresponsavel, PDO::PARAM_INT);
 $pre->execute();
-if ($pre->rowCount() == 0) {
-    header(':', true, 404);
-    header('X-PHP-Response-Code: 404', true, 404);
-    die(0);
+if ($pre->rowCount() > 0) {
+    
+    $sql1 = "update tbvinculados set  nome=:nome, nascimento=:nascimento, tipo=:tipo, lembrar=:lembrar where id_vinculado=:idvinculado";
+
+    $pre1 = $connPDO->prepare($sql1);
+    $pre1->bindParam(':nome', $nome, PDO::PARAM_STR);
+    $pre1->bindParam(':nascimento', $nascimento, PDO::PARAM_STR);
+    $pre1->bindParam(':tipo', $vinculo, PDO::PARAM_STR);
+    $pre1->bindParam(':lembrar', $lembrar, PDO::PARAM_INT);
+    $pre1->bindParam(':idvinculado', $idvinculado, PDO::PARAM_INT);
+
+    $pre1->execute();
+
+    $sql2 = "update tbentrada set perfil_acesso=:pacote, autoriza=0 where id_entrada=:identrada";
+
+    $pre2 = $connPDO->prepare($sql2);
+    $pre2->bindParam(':pacote', $pacote, PDO::PARAM_INT);
+    $pre2->bindParam(':identrada', $identrada, PDO::PARAM_INT);
+
+    $pre2->execute();
 }
 
 
-$sql1 = "update tbvinculados set  nome=:nome, nascimento=:nascimento, tipo=:tipo, lembrar=:lembrar where id_vinculado=:idvinculado";
-
-$pre1 = $connPDO->prepare($sql1);
-$pre1->bindParam(':nome', $nome, PDO::PARAM_STR);
-$pre1->bindParam(':nascimento', $nascimento, PDO::PARAM_STR);
-$pre1->bindParam(':tipo', $vinculo, PDO::PARAM_STR);
-$pre1->bindParam(':lembrar', $lembrar, PDO::PARAM_INT);
-$pre1->bindParam(':idvinculado', $idvinculado, PDO::PARAM_INT);
-
-$pre1->execute();
-
-$sql2 = "update tbentrada set perfil_acesso=:pacote, autoriza=0 where id_entrada=:identrada";
-
-$pre2 = $connPDO->prepare($sql2);
-$pre2->bindParam(':pacote', $pacote, PDO::PARAM_INT);
-$pre2->bindParam(':identrada', $identrada, PDO::PARAM_INT);
-
-$pre2->execute();
 ?>
