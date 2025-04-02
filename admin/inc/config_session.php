5524458758c163f3a1d6bd39d325f61dff7a6f30
tioale2018 <ale.tpd@gmail.com>
2025-04-01 23:30:22 -0300

    Update config_session.php

--- admin/inc/config_session.php
+++ admin/inc/config_session.php
@@ -7,7 +7,7 @@ ini_set('session.gc_maxlifetime', $inactive);
 
 // Define a duração do cookie de sessão para 30 minutos
 ini_set('session.cookie_lifetime', $inactive);
-
+/*
 // Definir parâmetros dos cookies de sessão
 $cookieParams = session_get_cookie_params();
 $cookieParams['httponly'] = true;
@@ -23,6 +23,7 @@ session_set_cookie_params(
     $cookieParams['secure'],
     $cookieParams['httponly']
 );
+*/
 session_start();
 /*
 session_write_close();
