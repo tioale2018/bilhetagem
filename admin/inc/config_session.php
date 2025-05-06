608c92230c175ec06fffa208a5cc12c2f0667d79
tioale2018 <ale.tpd@gmail.com>
2025-05-06 14:56:22 -0300

    Update config_session.php

--- admin/inc/config_session.php
+++ admin/inc/config_session.php
@@ -7,24 +7,23 @@ if (isset($_SESSION['evento']['tempo_tela'])) {
 }
 
 // Ajuste dos tempos
-// ini_set('session.gc_maxlifetime', $inactive);
-// ini_set('session.cookie_lifetime', $inactive);
+ini_set('session.gc_maxlifetime', $inactive);
+ini_set('session.cookie_lifetime', $inactive);
 
 // HTTPS seguro?
-// $isSecure = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off';
+$isSecure = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off';
 
 // Cookie de sessão
-// session_set_cookie_params([
-//     'lifetime' => $inactive,
-//     'path' => '/',
-//     'domain' => 'homologadev.com.br/',
-//     'secure' => $isSecure,
-//     'httponly' => true,
-//     'samesite' => 'Strict'
-// ]);
+session_set_cookie_params([
+    'lifetime' => $inactive,
+    'path' => '/',
+    'domain' => 'homologadev.com.br/',
+    'secure' => $isSecure,
+    'httponly' => true,
+    'samesite' => 'Strict'
+]);
 
 // Evita fixação de sessão
-
 // if (!isset($_SESSION['regenerate'])) {
 //     session_regenerate_id(true);
 //     $_SESSION['regenerate'] = true;
@@ -34,18 +33,18 @@ if (isset($_SESSION['evento']['tempo_tela'])) {
 $user_ip = $_SERVER['HTTP_CF_CONNECTING_IP'] ?? $_SERVER['REMOTE_ADDR'];
 
 // Proteção contra roubo de sessão (IP + User-Agent)
-// if (!isset($_SESSION['user_ip'], $_SESSION['user_agent'])) {
-//     $_SESSION['user_ip'] = $user_ip;
-//     $_SESSION['user_agent'] = $_SERVER['HTTP_USER_AGENT'];
-// } elseif (
-//     $_SESSION['user_ip'] !== $user_ip ||
-//     $_SESSION['user_agent'] !== $_SERVER['HTTP_USER_AGENT']
-// ) {
-//     session_unset();
-//     session_destroy();
-//     header('Location: /admin/');
-//     exit();
-// }
+if (!isset($_SESSION['user_ip'], $_SESSION['user_agent'])) {
+    $_SESSION['user_ip'] = $user_ip;
+    $_SESSION['user_agent'] = $_SERVER['HTTP_USER_AGENT'];
+} elseif (
+    $_SESSION['user_ip'] !== $user_ip ||
+    $_SESSION['user_agent'] !== $_SERVER['HTTP_USER_AGENT']
+) {
+    session_unset();
+    session_destroy();
+    header('Location: /admin/');
+    exit();
+}
 
 
 // Timeout por inatividade
