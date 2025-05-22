b29b2cba728dd586625b3042452807938ad0ca6c
tioale2018 <ale.tpd@gmail.com>
2025-05-22 16:46:00 -0300

    pem

--- admin/entrada-nova.php
+++ admin/entrada-nova.php
@@ -65,7 +65,19 @@
 <script src="./js/safe.js"></script>
 <script src="./js/operacional.js"></script>
 <script>
-
+if (typeof publicKeyPEM === 'undefined') {
+            const publicKeyPEM = `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0BxUXjrrGvXDCIplSQ7l
+XfPN1PHujl9CTumnjnM58/2vCtkEaqNbVMXbqhFbqSIpbd1J2k6nn9QMyEvA2uLe
+kVgQhMBhxtxFNnuMYWJAeLddas1+Vhn5jygLhdk+PxZSXi/ZKrrCqq1QwA+PSeRq
+aL4StVkBNCaxXRElxWXjsPVm0JUgXAuAfzBwGeKwelSUjgoTAmTLcNOOxDL+LGYD
+x7IM5PjofaiJwLj3oQpkcfsxvDZ3SMpj/Jo+V+i8OBQwCyVOAfOEvUN+O1YZlBUT
+LcM7KvDLMtcQyGf//3QsjLsfqa/XEAvdAISjHO5TNAXy9MXPiEwd1cPyis7toz/d
+mQIDAQAB
+-----END PUBLIC KEY-----`;
+            
+        }
+        
 $(document).ready(function(){
     $('#entrada-nova-lista').load('./blocos/entrada-nova-lista.php', { i: 1 });
 
