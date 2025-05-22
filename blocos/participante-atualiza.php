f24ecbe5a0e5ee1f1d8079955ac5a1d89c05c71e
tioale2018 <ale.tpd@gmail.com>
2025-05-22 00:29:47 -0300

    Update participante-atualiza.php

--- blocos/participante-atualiza.php
+++ blocos/participante-atualiza.php
@@ -1,7 +1,7 @@
 <?php
 // echo strlen($_POST['nome']) . " chars in nome\n";
 // echo "<hr>";
-die(var_dump($_POST));
+// die(var_dump($_POST));
 
 // require '../../vendor/autoload.php';
 
@@ -175,17 +175,10 @@ $encrypted_idvinculado_b64 = $_POST['idvinculado'] ?? '';
 $encrypted_identrada_b64 = $_POST['identrada'] ?? '';
 
 
-$encrypted_vinculo_b64 = $_POST['vinculo'] ?? '';
-$encrypted_pacote_b64 = $_POST['pacote'] ?? '';
-
-
-
-
-
 $encrypted_nome_raw = base64_decode($encrypted_nome_b64, true);
 $encrypted_nascimento_raw = base64_decode($encrypted_nascimento_b64, true);
-$encrypted_vinculo_raw = base64_decode($encrypted_vinculo_b64, true);
-$encrypted_pacote_raw = base64_decode($encrypted_pacote_b64, true);
+// $encrypted_vinculo_raw = base64_decode($encrypted_vinculo_b64, true);
+// $encrypted_pacote_raw = base64_decode($encrypted_pacote_b64, true);
 $encrypted_idresponsavel_raw = base64_decode($encrypted_idresponsavel_b64, true);
 $encrypted_idprevenda_raw = base64_decode($encrypted_idprevenda_b64, true);
 $encrypted_idvinculado_raw = base64_decode($encrypted_idvinculado_b64, true);
@@ -205,8 +198,6 @@ if (strlen($encrypted_nome_raw) !== 256) {
 try {
     $nome = $privateKey->decrypt($encrypted_nome_raw);
     $nascimento = $privateKey->decrypt($encrypted_nascimento_raw);
-    $vinculo = $privateKey->decrypt($encrypted_vinculo_raw);
-    $pacote = $privateKey->decrypt($encrypted_pacote_raw);
     $idresponsavel = $privateKey->decrypt($encrypted_idresponsavel_raw);
     $idprevenda = $privateKey->decrypt($encrypted_idprevenda_raw);
     $idvinculado = $privateKey->decrypt($encrypted_idvinculado_raw);
@@ -214,18 +205,6 @@ try {
 } catch (Exception $e) {
     die("Erro ao descriptografar: " . $e->getMessage());
 }
-
-die('nome: ' . $nome . "\n" . 
-    'nascimento: ' . $nascimento . "\n" . 
-    'vinculo: ' . $vinculo . "\n" . 
-    'pacote: ' . $pacote . "\n" . 
-    'idresponsavel: ' . $idresponsavel . "\n" . 
-    'idprevenda: ' . $idprevenda . "\n" . 
-    'idvinculado: ' . $idvinculado . "\n" .
-    'identrada: ' . $identrada
-);
-
-
 
 
 
@@ -287,6 +266,8 @@ try {
 
 
 $lembrar       = (isset($_POST['melembrar'])?1:0);
+$vinculo = $_POST['vinculo'] ?? '';
+$pacote  = $_POST['pacote'] ?? '';
 
 
 if ($_SERVER['REQUEST_METHOD']!="POST") {
