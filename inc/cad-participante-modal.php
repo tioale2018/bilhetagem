287a692d09b0a563e87159c6024318dafbed4a74
tioale2018 <ale.tpd@gmail.com>
2025-05-23 17:01:15 -0300

    Update cad-participante-modal.php

--- inc/cad-participante-modal.php
+++ inc/cad-participante-modal.php
@@ -82,18 +82,18 @@
 
 <script>
 if (typeof publicKeyPEM === 'undefined') {
-            var publicKeyPEM = `-----BEGIN PUBLIC KEY-----
-        MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0BxUXjrrGvXDCIplSQ7l
-        XfPN1PHujl9CTumnjnM58/2vCtkEaqNbVMXbqhFbqSIpbd1J2k6nn9QMyEvA2uLe
-        kVgQhMBhxtxFNnuMYWJAeLddas1+Vhn5jygLhdk+PxZSXi/ZKrrCqq1QwA+PSeRq
-        aL4StVkBNCaxXRElxWXjsPVm0JUgXAuAfzBwGeKwelSUjgoTAmTLcNOOxDL+LGYD
-        x7IM5PjofaiJwLj3oQpkcfsxvDZ3SMpj/Jo+V+i8OBQwCyVOAfOEvUN+O1YZlBUT
-        LcM7KvDLMtcQyGf//3QsjLsfqa/XEAvdAISjHO5TNAXy9MXPiEwd1cPyis7toz/d
-        mQIDAQAB
-        -----END PUBLIC KEY-----`;
-    }
+        var publicKeyPEM = `-----BEGIN PUBLIC KEY-----
+    MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0BxUXjrrGvXDCIplSQ7l
+    XfPN1PHujl9CTumnjnM58/2vCtkEaqNbVMXbqhFbqSIpbd1J2k6nn9QMyEvA2uLe
+    kVgQhMBhxtxFNnuMYWJAeLddas1+Vhn5jygLhdk+PxZSXi/ZKrrCqq1QwA+PSeRq
+    aL4StVkBNCaxXRElxWXjsPVm0JUgXAuAfzBwGeKwelSUjgoTAmTLcNOOxDL+LGYD
+    x7IM5PjofaiJwLj3oQpkcfsxvDZ3SMpj/Jo+V+i8OBQwCyVOAfOEvUN+O1YZlBUT
+    LcM7KvDLMtcQyGf//3QsjLsfqa/XEAvdAISjHO5TNAXy9MXPiEwd1cPyis7toz/d
+    mQIDAQAB
+    -----END PUBLIC KEY-----`;
+}
 
-    if (typeof pemToArrayBuffer === 'undefined') {        
+if (typeof pemToArrayBuffer === 'undefined') {        
     function pemToArrayBuffer(pem) {
         const b64 = pem
             .replace(/-----BEGIN PUBLIC KEY-----/, '')
@@ -116,6 +116,7 @@ if (typeof arrayBufferToBase64 === 'undefined') {
 }
 
 $(document).ready(function() {
+    alert('dois');
     // Função para calcular a idade com base na data de nascimento
     function calculateAge(date) {
         const today = new Date();
