6b25ff123bd595363a7712459c5047363ff7376a
tioale2018 <ale.tpd@gmail.com>
2025-05-23 11:46:11 -0300

    Update cad-participante-modal.php

--- inc/cad-participante-modal.php
+++ inc/cad-participante-modal.php
@@ -81,6 +81,18 @@
 <div id="idprevenda" data-id-prevenda="<?= $idPrevendaAtual ?>" style="display: none"></div>
 
 <script>
+if (typeof publicKeyPEM === 'undefined') {
+            var publicKeyPEM = `-----BEGIN PUBLIC KEY-----
+        MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0BxUXjrrGvXDCIplSQ7l
+        XfPN1PHujl9CTumnjnM58/2vCtkEaqNbVMXbqhFbqSIpbd1J2k6nn9QMyEvA2uLe
+        kVgQhMBhxtxFNnuMYWJAeLddas1+Vhn5jygLhdk+PxZSXi/ZKrrCqq1QwA+PSeRq
+        aL4StVkBNCaxXRElxWXjsPVm0JUgXAuAfzBwGeKwelSUjgoTAmTLcNOOxDL+LGYD
+        x7IM5PjofaiJwLj3oQpkcfsxvDZ3SMpj/Jo+V+i8OBQwCyVOAfOEvUN+O1YZlBUT
+        LcM7KvDLMtcQyGf//3QsjLsfqa/XEAvdAISjHO5TNAXy9MXPiEwd1cPyis7toz/d
+        mQIDAQAB
+        -----END PUBLIC KEY-----`;
+    }
+
     if (typeof pemToArrayBuffer === 'undefined') {        
     function pemToArrayBuffer(pem) {
         const b64 = pem
