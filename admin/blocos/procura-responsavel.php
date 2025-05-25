62120e7775868db2abb787492e3e50876f4ccf73
tioale2018 <ale.tpd@gmail.com>
2025-05-25 02:10:54 -0300

    Update procura-responsavel.php

--- admin/blocos/procura-responsavel.php
+++ admin/blocos/procura-responsavel.php
@@ -1,6 +1,6 @@
 <?php
 // die(var_dump($_POST));
-require '../vendor/autoload.php';
+require '../../vendor/autoload.php';
 
 use phpseclib3\Crypt\RSA;
 use phpseclib3\Crypt\PublicKeyLoader;
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD']!="POST") {
 }
 
 // Lê a chave privada
-$privateKey = PublicKeyLoader::loadPrivateKey(file_get_contents(__DIR__ . '/../chaves/chave_privada.pem'))
+$privateKey = PublicKeyLoader::loadPrivateKey(file_get_contents(__DIR__ . '/../../chaves/chave_privada.pem'))
     ->withPadding(RSA::ENCRYPTION_OAEP)
     ->withHash('sha256');
 
