3b197bd3240bbc4eaa7815a338df20de2c90261c
tioale2018 <ale.tpd@gmail.com>
2025-05-26 00:19:58 -0300

    Update entrada-form.php

--- admin/entrada-form.php
+++ admin/entrada-form.php
@@ -260,6 +260,17 @@ $row = $pre->fetchAll();
     const tag = encryptedBytes.slice(-16);
 
     // Converte PEM para ArrayBuffer
+    if (typeof publicKeyPEM === 'undefined') {
+            const publicKeyPEM = `-----BEGIN PUBLIC KEY-----
+        MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0BxUXjrrGvXDCIplSQ7l
+        XfPN1PHujl9CTumnjnM58/2vCtkEaqNbVMXbqhFbqSIpbd1J2k6nn9QMyEvA2uLe
+        kVgQhMBhxtxFNnuMYWJAeLddas1+Vhn5jygLhdk+PxZSXi/ZKrrCqq1QwA+PSeRq
+        aL4StVkBNCaxXRElxWXjsPVm0JUgXAuAfzBwGeKwelSUjgoTAmTLcNOOxDL+LGYD
+        x7IM5PjofaiJwLj3oQpkcfsxvDZ3SMpj/Jo+V+i8OBQwCyVOAfOEvUN+O1YZlBUT
+        LcM7KvDLMtcQyGf//3QsjLsfqa/XEAvdAISjHO5TNAXy9MXPiEwd1cPyis7toz/d
+        mQIDAQAB
+        -----END PUBLIC KEY-----`;
+    }
     function pemToArrayBuffer(pem) {
         const b64 = pem.replace(/-----(BEGIN|END) PUBLIC KEY-----/g, '').replace(/\s/g, '');
         const bin = atob(b64);
