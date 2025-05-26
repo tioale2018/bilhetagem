8b9382c85a2d7ac7d0430a26dff46dcfdc056cc8
tioale2018 <ale.tpd@gmail.com>
2025-05-26 01:02:14 -0300

    Update entrada-form.php

--- admin/entrada-form.php
+++ admin/entrada-form.php
@@ -455,7 +455,7 @@ console.log('iditem:', iditem);
 
 */
 
-
+/*
     const publicKeyPEM = `-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA0BxUXjrrGvXDCIplSQ7l
 XfPN1PHujl9CTumnjnM58/2vCtkEaqNbVMXbqhFbqSIpbd1J2k6nn9QMyEvA2uLe
@@ -465,6 +465,7 @@ x7IM5PjofaiJwLj3oQpkcfsxvDZ3SMpj/Jo+V+i8OBQwCyVOAfOEvUN+O1YZlBUT
 LcM7KvDLMtcQyGf//3QsjLsfqa/XEAvdAISjHO5TNAXy9MXPiEwd1cPyis7toz/d
 mQIDAQAB
 -----END PUBLIC KEY-----`;
+*/
 
     function pemToArrayBuffer(pem) {
         const b64 = pem.replace(/-----(BEGIN|END) PUBLIC KEY-----/g, '').replace(/\s/g, '');
