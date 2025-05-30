69c762e1316426e068f885cbc472ac511ab5d01e
tioale2018 <ale.tpd@gmail.com>
2025-05-29 22:46:14 -0300

    Update caixa-basico.php

--- admin/caixa-basico.php
+++ admin/caixa-basico.php
@@ -15,8 +15,8 @@ function generateSqlQuery($date) {
     }
     $startTimestamp = $dateTime->setTime(0, 0)->getTimestamp();
     $endTimestamp = $dateTime->setTime(23, 59, 59)->getTimestamp();
-    // $sql = "SELECT tbfinanceiro.*, tbprevenda.id_evento FROM tbfinanceiro inner join tbprevenda on tbfinanceiro.id_prevenda=tbprevenda.id_prevenda WHERE tbprevenda.id_evento=".$_SESSION['evento_selecionado']." and tbfinanceiro.ativo=1 AND tbfinanceiro.hora_pgto BETWEEN $startTimestamp AND $endTimestamp order by tbprevenda.id_prevenda desc, tbfinanceiro.hora_pgto desc";
-    $sql = "SELECT tbfinanceiro.*, tbprevenda.id_evento FROM tbfinanceiro inner join tbprevenda on tbfinanceiro.id_prevenda=tbprevenda.id_prevenda WHERE tbprevenda.id_evento=:id_evento and tbfinanceiro.ativo=1 AND tbfinanceiro.hora_pgto BETWEEN :startTimestamp AND :endTimestamp order by tbprevenda.id_prevenda desc, tbfinanceiro.hora_pgto desc";
+    $sql = "SELECT tbfinanceiro.*, tbprevenda.id_evento FROM tbfinanceiro inner join tbprevenda on tbfinanceiro.id_prevenda=tbprevenda.id_prevenda WHERE tbprevenda.id_evento=".$_SESSION['evento_selecionado']." and tbfinanceiro.ativo=1 AND tbfinanceiro.hora_pgto BETWEEN $startTimestamp AND $endTimestamp order by tbprevenda.id_prevenda desc, tbfinanceiro.hora_pgto desc";
+    // $sql = "SELECT tbfinanceiro.*, tbprevenda.id_evento FROM tbfinanceiro inner join tbprevenda on tbfinanceiro.id_prevenda=tbprevenda.id_prevenda WHERE tbprevenda.id_evento=:id_evento and tbfinanceiro.ativo=1 AND tbfinanceiro.hora_pgto BETWEEN :startTimestamp AND :endTimestamp order by tbprevenda.id_prevenda desc, tbfinanceiro.hora_pgto desc";
     return $sql;
 }
 
@@ -34,9 +34,9 @@ $varget = '';
     $idevento_selecionado = $_SESSION['evento_selecionado'];
     
     $pre_busca_pgto = $connPDO->prepare($sql_busca_pgto);
-    $pre_busca_pgto->bindParam(':id_evento', $idevento_selecionado, PDO::PARAM_INT);
-    $pre_busca_pgto->bindParam(':startTimestamp', $startTimestamp, PDO::PARAM_STR);
-    $pre_busca_pgto->bindParam(':endTimestamp', $endTimestamp, PDO::PARAM_STR);
+    // $pre_busca_pgto->bindParam(':id_evento', $idevento_selecionado, PDO::PARAM_INT);
+    // $pre_busca_pgto->bindParam(':startTimestamp', $startTimestamp, PDO::PARAM_STR);
+    // $pre_busca_pgto->bindParam(':endTimestamp', $endTimestamp, PDO::PARAM_STR);
     $pre_busca_pgto->execute();
     $row_busca_pgto = $pre_busca_pgto->fetchAll();
 
