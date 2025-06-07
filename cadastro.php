1b28e5908190f55f754a4335fed545be0ac69d71
tioale2018 <ale.tpd@gmail.com>
2025-06-06 22:59:23 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -37,6 +37,79 @@ include_once("./inc/funcoes.php");
     z-index: 999;
     display: flex;
 }
+
+.switch {
+  display: inline-block;
+  height: 34px;
+  position: relative;
+  width: 60px;
+}
+
+.switch input {
+  display:none;
+}
+
+.slider {
+  background-color: #ccc;
+  bottom: 0;
+  cursor: pointer;
+  left: 0;
+  position: absolute;
+  right: 0;
+  top: 0;
+  transition: .4s;
+}
+
+.slider:before {
+  background-color: #fff;
+  bottom: 4px;
+  content: "";
+  height: 26px;
+  left: 4px;
+  position: absolute;
+  transition: .4s;
+  width: 26px;
+}
+
+input:checked + .slider {
+  background-color: #66bb6a;
+}
+
+input:checked + .slider:before {
+  transform: translateX(26px);
+}
+
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
+
+
+
+
+/* general styling */
+html, body {
+  height: 100%;
+  margin: 0;
+}
+
+body {
+  -webkit-box-align: center;
+  -ms-flex-align: center;
+  align-items: center;
+  background-color: #f1f2f3;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+}
+
+.container {
+  margin: 0 auto;
+}
 </style>
 </head>
 <body class="theme-black">
@@ -99,6 +172,17 @@ include_once("./inc/funcoes.php");
                                     <tr>
                                         <td>E-mail:</td>
                                         <td><?= $dados_responsavel[0]['email'] ?></td>
+                                    </tr>
+                                    <tr>
+                                        <td>Participante</td>
+                                        <td>
+                                            <div class="container">
+                                                <label class="switch" for="checkbox">
+                                                    <input type="checkbox" id="checkbox" />
+                                                    <div class="slider round"></div>
+                                                </label>
+                                            </div>
+                                        </td>
                                     </tr>
                                 </table>
                             </div>
