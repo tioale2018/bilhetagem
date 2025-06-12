78f6e37e6c77649c4cf83bd5ae066a48019fc852
tioale2018 <ale.tpd@gmail.com>
2025-06-12 14:00:57 -0300

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -85,9 +85,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 echo "<script>alert('Perfil: ".$_SESSION['user_perfil']."');</script>";
 
-/*
-//if (isset($_SESSION['user_id']) && $_SESSION['user_perfil']==1) { -- versão original
-if (isset($_SESSION['user_id'])) { //versão pra resolver o problema de redirecionamento
+
+if (isset($_SESSION['user_id']) && $_SESSION['user_perfil']==1) { //-- versão original
+// if (isset($_SESSION['user_id'])) { //versão pra resolver o problema de redirecionamento
     // Se estiver definida, redireciona para a página do sistema
     header('Location: controle.php');
     exit();
@@ -100,7 +100,7 @@ if (isset($_SESSION['user_id']) && $_SESSION['user_perfil']==2) {
     // header('Location: ../master');
     exit();
 }
-*/
+
 ?>
 <!doctype html>
 <html class="no-js " lang="pt-br">
