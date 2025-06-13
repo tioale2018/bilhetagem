d8c37ad89618d5e7c746fa16ba37853db9f1d1f7
tioale2018 <ale.tpd@gmail.com>
2025-06-12 22:27:16 -0300

    vinculo modal

--- inc/cad-participante-modal.php
+++ inc/cad-participante-modal.php
@@ -4,7 +4,7 @@
         <div class="modal-content">
             <form action="" method="post" id="formModalAddParticipante">
                 <div class="modal-header">
-                    <h4 class="title" id="modalAddParticipanteLabel">Adicionar participantes</h4>
+                    <h4 class="title" id="modalAddParticipanteLabel">Adicionar participante</h4>
                 </div>
                 <div class="modal-body"> 
                     <div class="row clearfix">
@@ -22,7 +22,7 @@
                         </div>
                         <div class="col-md-4">
                             <div class="form-group">
-                                <label for="fvinculo" class="form-label">Tipo de vínculo</label>                            
+                                <label for="fvinculo" class="form-label">Vínculo com respon. principal</label>                            
                                 <select name="vinculo" class="form-control show-tick p-0" name="vinculo" id="fvinculo">
                                     <option value="">Escolha</option>
                                     <?php foreach ($_SESSION['lista_vinculos'] as $k => $v) { ?>
