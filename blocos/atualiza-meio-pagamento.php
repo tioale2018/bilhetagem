ae3337bf6dc859b9bd86814db923453326b184f7
tioale2018 <ale.tpd@gmail.com>
2025-06-13 03:20:37 -0300

    Update atualiza-meio-pagamento.php

--- blocos/atualiza-meio-pagamento.php
+++ blocos/atualiza-meio-pagamento.php
@@ -1,5 +1,5 @@
 <?php
-die(var_dump($_POST));
+// die(var_dump($_POST));
 require '../../vendor/autoload.php';
 
 use phpseclib3\Crypt\RSA;
@@ -19,15 +19,12 @@ session_start();
 
 // $idresponsavel = $_SESSION['dadosResponsavel'][0]['id_responsavel'];
 
-$encrypted_nome      = base64_decode($_POST['nome_seguro'] ?? '');
-$encrypted_telefone = base64_decode($_POST['telefone_seguro'] ?? '');
-$encrypted_cpf      = base64_decode($_POST['cpf_seguro'] ?? '');
+$encrypted_meioPagamento      = base64_decode($_POST['meioPagamento'] ?? '');
+
 
 try {
-    $nome     = $privateKey->decrypt($encrypted_nome);
-    $telefone = $privateKey->decrypt($encrypted_telefone);
-    $cpf      = $privateKey->decrypt($encrypted_cpf);
-    
+    $meioPagamento = $privateKey->decrypt($encrypted_meioPagamento);
+
 } catch (Exception $e) {
     die ("Erro ao descriptografar: " . $e->getMessage());
 }
@@ -41,43 +38,34 @@ include('../inc/funcoes.php');
 $idPrevendaAtual   = $_SESSION['idPrevenda'];
 
 
-$sql_secundario = "SELECT * from tbsecundario WHERE ativo=1 and idprevenda = :idPrevenda";
-$pre_secundario = $connPDO->prepare($sql_secundario);
-$pre_secundario->bindValue(':idPrevenda', $idPrevendaAtual, PDO::PARAM_INT);
-$pre_secundario->execute();
+$sql_prevenda_info = "SELECT * FROM tbprevenda_info WHERE ativo=1 and idprevenda = :idPrevenda";
+$pre_prevenda_info = $connPDO->prepare($sql_prevenda_info);
+$pre_prevenda_info->bindValue(':idPrevenda', $idPrevendaAtual, PDO::PARAM_INT);
+$pre_prevenda_info->execute();
 
-if ($pre_secundario->rowCount() > 0) {
-    // Se já existe um responsável secundário, atualiza os dados
-    $dados_secundario = $pre_secundario->fetchAll(PDO::FETCH_ASSOC);
-    $idSecundario = $dados_secundario[0]['id'];
-
-    $sql_atualiza_secundario = "UPDATE tbsecundario SET nome=:nome, cpf=:cpf, telefone=:telefone, lastupdate=:lastupdate WHERE id=:id";
-    $pre_atualiza_secundario = $connPDO->prepare($sql_atualiza_secundario);
-    $pre_atualiza_secundario->bindParam(':nome', $nome, PDO::PARAM_STR);
-    $pre_atualiza_secundario->bindParam(':cpf', $cpf, PDO::PARAM_STR);
-    $pre_atualiza_secundario->bindParam(':telefone', $telefone, PDO::PARAM_STR);
-    $pre_atualiza_secundario->bindParam(':lastupdate', $horaagora, PDO::PARAM_INT);
-    $pre_atualiza_secundario->bindParam(':id', $idSecundario, PDO::PARAM_INT);
-    $pre_atualiza_secundario->execute();
-
+if ($pre_prevenda_info->rowCount() > 0) {
+    $row_prevendainfo = $pre_prevenda_info->fetchAll(PDO::FETCH_ASSOC);
+    $id_prevendainfo = $row_prevendainfo[0]['id'];
+    //se ja existir atualiza
+    $sql_atualiza_prevenda = "UPDATE tbprevenda_info SET meiopgto=:meioPagamento WHERE id=:idPrevenda";
+    $pre_atualiza_prevenda = $connPDO->prepare($sql_atualiza_prevenda);
+    $pre_atualiza_prevenda->bindParam(':meioPagamento', $meioPagamento, PDO::PARAM_STR);
+    $pre_atualiza_prevenda->bindParam(':idPrevenda', $id_prevendainfo, PDO::PARAM_INT);
+    $pre_atualiza_prevenda->execute();
     //echo ok em json
     header('Content-Type: application/json');
-    echo json_encode(['status' => 'ok', 'message' => 'Responsável legal atualizado com sucesso.']);
+    echo json_encode(['status' => 'ok', 'message' => 'Atualizado com sucesso.']);
+
 } else {
-    // Caso contrário, cria um novo responsável secundário
-    $sql_insere_secundario = "INSERT INTO tbsecundario (nome, cpf, telefone, idprevenda, lastupdate) VALUES (:nome, :cpf, :telefone, :idPrevenda, :lastupdate)";
-    $pre_insere_secundario = $connPDO->prepare($sql_insere_secundario);
-    $pre_insere_secundario->bindParam(':nome', $nome, PDO::PARAM_STR);
-    $pre_insere_secundario->bindParam(':cpf', $cpf, PDO::PARAM_STR);
-    $pre_insere_secundario->bindParam(':telefone', $telefone, PDO::PARAM_STR);
-    $pre_insere_secundario->bindParam(':idPrevenda', $idPrevendaAtual, PDO::PARAM_INT);
-    $pre_insere_secundario->bindParam(':lastupdate', $horaagora, PDO::PARAM_INT);
-    $pre_insere_secundario->execute();
-
+    // Se não existir, cria uma nova entrada
+    $sql_insere_prevenda = "INSERT INTO tbprevenda_info (idprevenda, meiopgto) VALUES (:idPrevenda, :meioPagamento)";
+    $pre_insere_prevenda = $connPDO->prepare($sql_insere_prevenda);
+    $pre_insere_prevenda->bindParam(':idPrevenda', $idPrevendaAtual, PDO::PARAM_INT);
+    $pre_insere_prevenda->bindParam(':meioPagamento', $meioPagamento, PDO::PARAM_STR);
+    $pre_insere_prevenda->execute();
     //echo ok em json
     header('Content-Type: application/json');
-    echo json_encode(['status' => 'ok', 'message' => 'Responsável legal adicionado com sucesso.']);
+    echo json_encode(['status' => 'ok', 'message' => 'Inserido com sucesso.']);
 }
-
 
 ?>
