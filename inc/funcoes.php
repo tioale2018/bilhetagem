e2f7c85df01b7b31fe506dcafa22144cf775d989
tioale2018 <ale.tpd@gmail.com>
2025-06-12 21:47:26 -0300

    Update funcoes.php

--- inc/funcoes.php
+++ inc/funcoes.php
@@ -16,7 +16,8 @@ function formatarCPF($cpf) {
 
 function procuraResponsavel($i) {
     $id              = limparCPF($i); //$GLOBALS['id'];
-    $sql_responsavel = "select * from tbresponsavel where cpf=:cpf";
+    // $sql_responsavel = "select * from tbresponsavel where cpf=:cpf";
+    $sql_responsavel = "select tbresponsavel.*, tbvinculoresponsavel.descricao from tbresponsavel inner join tbvinculoresponsavel on tbresponsavel.vinculo = tbvinculoresponsavel.id where cpf=:cpf";
     $pre_responsavel = $GLOBALS['connPDO']->prepare($sql_responsavel);
     $pre_responsavel->bindParam(':cpf', $id, PDO::PARAM_INT);
     $pre_responsavel->execute();
