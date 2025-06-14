b6ff464dda9a89800463fa45708be5f48111586b
tioale2018 <ale.tpd@gmail.com>
2025-06-14 01:15:58 -0300

    Update termo-exibe.php

--- admin/termo-exibe.php
+++ admin/termo-exibe.php
@@ -246,7 +246,7 @@ body {
 </table>
 <?= replaceVariables($row_busca_termo['textotermo'], $variables); ?>
 
-<div style="border-top: 1px solid #000; margin-top: 20px; padding: 0 10px;">
+<div style="border-top: 1px solid #000; margin: 20px 0; padding: 0 10px;">
     <p style="text-align: center;">Assinatura do responsável principal</p>
 
 </div>
